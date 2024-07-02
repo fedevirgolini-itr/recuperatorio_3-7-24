77456a50ebb4798d73170755e0d3f79b93b108fd
Federico Vrigolini <fvirgolini@itr.edu.ar>
2024-07-02 19:52:24 -0300

    esqueleto evaluación

---
+++ src/Ejercicio1.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function Ejercicio1() {
+  return (
+    <div>
+      <h2>Ejercicio 1: Formulario y lista desplegable</h2>
+
+      {/* Implementar formulario */}
+
+    </div>
+  )
+}
