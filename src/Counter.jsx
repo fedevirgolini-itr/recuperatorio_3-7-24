df527b4eaf734a8eef4c852b740d914fb85c8f8f
SantiMacch <d48070187@alumnos.itr.edu.ar>
2024-07-03 10:49:47 -0300

    Prueba terminada

---
+++ src/Counter.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Counter() {
+  return (
+    <div>
+
+      {/* Renderizar componentes implementadas */}
+
+    </div>
+  )
+}
--- src/Ejercicio3.jsx
+++ src/Ejercicio3.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
+import Counter from './Counter'
 
 export default function Ejercicio3() {
   return (
     <div>
       <h2>Ejercicio 3: Modularización y gestión de eventos</h2>
 
-      {/* Renderizar componentes implementadas */}
+      {<> <Counter/> </>}
 
     </div>
   )
